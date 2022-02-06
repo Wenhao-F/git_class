cebb244874ecf0f9b5fbc95dfd5f1304ebbe257b
Wenhao Fu <wenhao.fu@gmail.com>
2022-02-06 17:42:06 +0000

    new readme and file premission

---
+++ README.md
@@ -0,0 +1,6 @@
+# Week 2 Lab – Bash & Git
+Wenhao Fu
+12345
+GitTest(https://github.com/Wenhao-F/git_class.git)
+---
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -6,15 +6,21 @@ mkdir fol_2
 cd fol_1
 
 touch 1_1.txt
+chmod 600 1_1.txt
 touch 1_2.txt
+chmod 777 1_2.txt
 touch 1_3.txt
+chmod 600 1_3.txt
 
 cd ..
 cd fol_2
 
 touch 2_1.txt
+chmod 600 2_1.txt
 touch 2_2.txt
+chmod 777 2_2.txt
 touch 2_3.txt
+chmod 600 2_3.txt
 
 cd ..
 rm -r fol_1/
